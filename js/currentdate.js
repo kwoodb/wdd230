2c03fc5f2e67bd1571a88dd77478030b2e360096
kwoodb <woo07003@byui.edu>
2021-09-16 14:16:30 -0700

    commit

---
+++ js/currentdate.js
@@ -0,0 +1,6 @@
+const copyrightyear = document.querySelector("#copyrightyear");
+copyrightyear.textContent = new Date().getFullYear();
+
+const lastupdated = document.lastModified;
+document.getElementById("lastupdated").textContent = lastupdated;
+
