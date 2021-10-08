a8f239cfda3ce971a9e3f6d4bc4c0413e1172681
kwoodb <woo07003@byui.edu>
2021-10-08 15:54:48 -0700

    commit

---
+++ lesson4/js/dates.js
@@ -0,0 +1,11 @@
+let d = new Date();
+document.getElementById("copyrightyear").textContent = d.getFullYear();
+
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+}
+
+document.getElementById("currentDate").textContent = d.toLocaleDateString('en-UK', options);
