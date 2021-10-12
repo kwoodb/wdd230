522fb5948b63c19b6a882815580dc57b29a0a991
kwoodb <woo07003@byui.edu>
2021-10-12 13:47:45 -0700

    commmit

--- lesson4update/js/hamburger.js
+++ lesson4update/js/hamburger.js
@@ -1,10 +1,10 @@
 
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hambutton.addEventListener("click", () => {mainnav.classList.toggle("responsive")}, false);
 
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+window.onresize = () => {if (window.innerWidth > 520) mainnav.classList.remove('responsive')};
 
 /*function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList("responsive");
