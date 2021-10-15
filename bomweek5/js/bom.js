5e037921fa04cadf2636f607d12959ead67521ef
kwoodb <woo07003@byui.edu>
2021-10-14 18:09:46 -0700

    commit

--- bomweek5/js/bom.js
+++ bomweek5/js/bom.js
@@ -1,3 +1,35 @@
-const input = document.querySelector(".input");
-const submit = document.querySelector(".submit");
-const chapt = document.querySelector(".list");
+const input = document.querySelector('input');
+        const button = document.querySelector('button');
+        const list = document.querySelector('.list');
+        const main = document.querySelector('main');
+        const p = document.createElement('p');
+
+        button.addEventListener('click', function() {
+            let userInput =  input.value;
+
+            if (userInput == ""){
+                p.textContent ='Please enter a Book of Mormon chapter.';
+                main.appendChild(p);
+                p.style.display = 'block';
+        }
+            
+            else {    
+                const listItem = document.createElement('li');
+                const deleteBtn = document.createElement('button');
+                listItem.textContent = userInput;
+                deleteBtn.textContent = "\u274C";
+                listItem.appendChild(deleteBtn);
+                list.appendChild(listItem);
+                console.log(p);
+                p.style.display = 'none';
+                deleteBtn.addEventListener('click',function() {
+                list.removeChild(listItem);
+                
+            })
+            }
+        input.focus();
+        input.value = "";
+        })
+
+            
+        
