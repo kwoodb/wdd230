dd851c899b9d3dfc16f90c5ce097fd66efd42062
kwoodb <woo07003@byui.edu>
2021-10-28 14:19:27 -0700

    commit

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,52 +1,35 @@
-const images = document.querySelectorAll("img[data-src]");
-function preloadImage(img) {
-  const src = img.getAttribute("data-src");
-  if (!src) {
-    return;
-  }
-  img.src = src;
-}
-const imgOptions = {
-  threshold: 0,
-  rootMargin: "0px 0px 300px 0px",
-};
+const seenImages = document.querySelectorAll("img[data-src]");
 
-if ("IntersectionObserver" in window) {
-  const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) {
-        return;
-      } else {
-        preloadImage(entry.target);
-        imgObserver.unobserve(entry.target);
-      }
-    });
-  }, imgOptions);
-
-  images.forEach((image) => {
-    imgObserver.observe(image);
-  });
-} else {
-    images.forEach((image) => {
-        preloadImage(image);
-      });
-}
-/*const imagesToLoad = document.querySelectorAll("img[data-src]");
 const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
+  threshold: 1,
+  rootMargin: '0px 0px 500px 0px'
 };
 
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {image.removeAttribute('data-src');};
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  }
 };
 
-if('IntersectionObserver' in window) {
+if ('InstersectionObserver' in window) {
+  const imgObserver = new IntersectionObserver((items, imgObserver) => {
+    items.forEach(item => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
+    })
+  }, imgOptions);
 
+  seenImages.forEach(img => {
+    imgObserver.observe(img);
+  });
 }
 else {
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    });
-}*/
+  seenImages.forEach(img => {
+    loadImages(img);
+  });
+}
+
+
